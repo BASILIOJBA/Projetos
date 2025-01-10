9ab72b9d891ae692842fe8e2c799eb3493731cc8
Joaquim Basilio Alves <95645412+BASILIOJBA@users.noreply.github.com>
2025-01-09 21:01:10 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="JBicon.ico" type="image/x-icon">
+    <title>Index da Loja</title>
+</head>
+<body>
+    <header>
+        <h1>Cabeçalho da loja</h1>
+    </header>
+    <nav>
+        <h1>Corpo da loja</h1>
+        <?php 
+        echo "\u{1f30e}";
+        ?>
+    </nav>
+    <footer>
+        <h1>Rodapé</h1>
+    </footer> 
+</body>
+</html>
